38cbff9fe3ea76aa23c164c861963c9d0110b7b5
MAX999 <max999b@gmail.com>
2023-03-28 22:48:38 +0300

    1
    розмітка

--- src/hw7/src/js/01-gallery.js
+++ src/hw7/src/js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEr = document.querySelector('.gallery');
+
+// Функція генерування розмітки масиву зображень
+// зупинка стандартної поведінки браузера onclick="event.preventDefault()"
+// onst link = document.querySelector('a'); // отримуємо посилання з документу
+// link.addEventListener('click', function(event) {
+//   event.preventDefault(); // зупиняємо стандартну поведінку браузера
+// робимо власний код тут
+// });
+function createLi(array) {
+  return array.reduce((acc, item) => `${acc} 
+            <li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg" onclick="event.preventDefault()"> 
+        <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+        />
+    </a>
+    </li> `, '');
+}
+const LiErray = createLi(galleryItems);
+// додавання в дом дерева
+galleryEr.insertAdjacentHTML('beforeend', LiErray);
+
+// навішування слухачів подій
+function listHandler(e) {
+  console.log(e.target);
+  const descripUrl = e.target.dataset.source;
+  console.log(descripUrl);
+}
+galleryEr.addEventListener('click', listHandler);
