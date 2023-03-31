8617840e396c316282992d61b506391757748444
MAX999 <max999b@gmail.com>
2023-03-31 22:24:15 +0300

    1 task
    1 завдання

--- src/hw7/src/js/01-gallery.js
+++ src/hw7/src/js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEr = document.querySelector('.gallery');
 
+
 // Функція генерування розмітки масиву зображень
 // зупинка стандартної поведінки браузера onclick="event.preventDefault()"
 // onst link = document.querySelector('a'); // отримуємо посилання з документу
@@ -28,9 +29,31 @@ const LiErray = createLi(galleryItems);
 galleryEr.insertAdjacentHTML('beforeend', LiErray);
 
 // навішування слухачів подій
+let instance = null;  // локальна зміна для збільшення області видимості instance
+
+// функція Слухач події
 function listHandler(e) {
   console.log(e.target);
-  const descripUrl = e.target.dataset.source;
+  const descripUrl = e.target.dataset.source; // присвоєння властивості по дата обєкту
   console.log(descripUrl);
+
+  // Модуль відкривання і створення модалього вікна
+  instance = basicLightbox.create(`
+     <img src=${descripUrl} > `);
+
+  instance.show();
+  console.log(instance);
 }
+// функція закривання
+function closeModal() {
+  instance.close();
+}
+// визов функції події на натиск
 galleryEr.addEventListener('click', listHandler);
+
+// визов функції закриття з івентом esc
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+});
